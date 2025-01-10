c9b40d94f20c7bca7c9194f423e42b83af4a5575
AnkitCLI <ankit.yadav@cloudsufi.com>
2025-01-10 12:14:37 +0530

    BigQuery Error Message Changes

--- src/e2e-test/java/io/cdap/plugin/utils/E2ETestConstants.java
+++ src/e2e-test/java/io/cdap/plugin/utils/E2ETestConstants.java
@@ -4,6 +4,7 @@ package io.cdap.plugin.utils;
  * E2E Test constants.
  */
 public class E2ETestConstants {
+  public static final String ERROR_MSG_INCORRECT_DATASET_NAME = "errorMessageIncorrectDatasetName";
   public static final String ERROR_MSG_GCS_INVALID_PATH = "errorMessageGCSInvalidPath";
   public static final String ERROR_MSG_GCS_INVALID_BUCKET_NAME = "errorMessageGCSInvalidBucketName";
   public static final String ERROR_MSG_INCORRECT_TABLE = "errorMessageIncorrectBQTable";
